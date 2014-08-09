81c27ccfd91625c890d22ef16ddc6bbf1556fb8e
Yelnil Gabo <yelnil@gmail.com>
2014-08-08 17:11:17 -0700

    Fix chapter API.
    Can now display subevents by chapter.

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"strconv"
 
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 )
 
 var (
-	JSON_FILE   = "data/subevent.json"
-	blankEvents []Chapter
+	JSON_FILE       = "data/subevent.json"
+	eventsByChapter []Chapter
 )
 
 type Subevent struct {
@@ -25,6 +27,7 @@ type Subevent struct {
 type Chapter struct {
 	Chapter   int         `json:"chapter"`
 	Title     string      `json:"title"`
+	Period    string      `json:"period"`
 	Location  string      `json:"location"`
 	Subevents *[]Subevent `json:"subevents"`
 }
@@ -39,18 +42,49 @@ func updateCache() {
 	dat, err := ioutil.ReadFile(JSON_FILE)
 	check(err)
 
-	err = json.Unmarshal(dat, &blankEvents)
+	err = json.Unmarshal(dat, &eventsByChapter)
 	check(err)
 }
 
+func getChapter(chapter int) Chapter {
+	for _, current := range eventsByChapter {
+		if current.Chapter == chapter {
+			return current
+		}
+	}
+	return Chapter{}
+}
+
+func indexHandler(r render.Render) {
+	r.HTML(200, "index", nil)
+}
 func main() {
 	updateCache()
 	m := martini.Classic()
-	m.Get("/", func() string {
-		return "Hello world!"
+
+	templateOptions := render.Options{}
+	templateOptions.Delims.Left = "#{"
+	templateOptions.Delims.Right = "}#"
+	m.Use(render.Renderer(templateOptions))
+
+	m.Get("/", indexHandler)
+
+	m.Get("/chapter", func() string {
+		kk, _ := json.Marshal(eventsByChapter)
+		return string(kk)
 	})
-	m.Get("/events.json", func() string {
-		kk, _ := json.Marshal(blankEvents)
+	m.Get("/chapter/:id", func(params martini.Params) string {
+		var chap Chapter
+		if i, err := strconv.Atoi(params["id"]); err == nil {
+			chap = getChapter(i)
+		} else {
+			chap = Chapter{}
+		}
+		kk, _ := json.Marshal(chap)
+		return string(kk)
+	})
+	m.Get("/subevent", func() string {
+		kk, _ := json.Marshal(eventsByChapter)
 		return string(kk)
 	})
 	m.Run()
